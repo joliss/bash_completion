0ea8fcb1bc0300e9baa1b208215dc9997383a7d3
Dawid Ciężarkiewicz <dpc@ucore.info>
2011-04-07 21:07:08 +0200

    Update update-bash-completion.sh

--- update-bash-completion.sh
+++ update-bash-completion.sh
@@ -3,10 +3,10 @@
 cd /etc/bash_completion.d || exit 1
 
 for i in *; do
-	[ -L "$i" ] && {
-		rm "$i"
-		continue
-	}
+	[ -L "$i" ] && rm "$i"
+done
+
+for i in *; do
 	for com in `cat $i | grep 'complete -F' | awk  '{print $NF}' | fgrep -v '|' | fgrep -v '$' | grep -v '&'`; do
 		if [ "$com" != "$i" ]; then
 			echo "$com -> $i"
